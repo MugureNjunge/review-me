3113f8ef431bfcc2206023e4c335d7ef3767f47e
MugureNjunge <maureennjunge22@gmail.com>
2022-06-12 10:10:30 +0300

    create my index route

---
+++ theawwards/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('',views.index, name='index'),
+
+]    

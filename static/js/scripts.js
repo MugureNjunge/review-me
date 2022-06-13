c6436ce3e61cc718ee244c8177c476b0797c69f5
MugureNjunge <maureennjunge22@gmail.com>
2022-06-13 19:20:07 +0300

    add for loop to my index page to enable user view various projects

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,29 +1,55 @@
-$(document).ready(function () {
+// $(document).ready(function () {
 
-  // hover over landing page
-  $('.landing').hover(function () {
-      // over
-      $('.landing-abs-fade').addClass('dark-fade');
-      $('.landing-abs-fade').removeClass('light-fade');
-  }, function () {
-      // out
-      $('.landing-abs-fade').addClass('light-fade');
-      $('.landing-abs-fade').removeClass('dark-fade');
-  });
+//   // hover over landing page
+//   $('.landing').hover(function () {
+//       // over
+//       $('.landing-abs-fade').addClass('dark-fade');
+//       $('.landing-abs-fade').removeClass('light-fade');
+//   }, function () {
+//       // out
+//       $('.landing-abs-fade').addClass('light-fade');
+//       $('.landing-abs-fade').removeClass('dark-fade');
+//   });
 
-  // hover over individual website box
-});
+//   // hover over individual website box
+// });
 
-function getid(fadeId) {
-  // over
-  let fade = '#fade' + fadeId
-  $(fade).addClass('site-fade-in');
-  $(fade).removeClass('site-fade-out');
-}
+// function getid(fadeId) {
+//   // over
+//   let fade = '#fade' + fadeId
+//   $(fade).addClass('site-fade-in');
+//   $(fade).removeClass('site-fade-out');
+// }
 
-function outmouse(fadeId) {
-  // out
-  let fade = '#fade' + fadeId
-  $(fade).removeClass('site-fade-in');
-  $(fade).addClass('site-fade-out');
-};
+// function outmouse(fadeId) {
+//   // out
+//   let fade = '#fade' + fadeId
+//   $(fade).removeClass('site-fade-in');
+//   $(fade).addClass('site-fade-out');
+// };
+
+
+
+
+
+$(document).ready(function() {
+  $('form').submit(function(event) {
+          event.preventDefault()
+          form = $("form")
+
+          $.ajax({
+                  'url': '/ajax/register/',
+                  'type': 'POST',
+                  'data': form.serialize(),
+                  'dataType': 'json',
+                  'success': function(data) {
+                      alert(data['success'])
+                  },
+              }) // END of Ajax method
+          $('#id_username').val('')
+          $("#id_email").val('')
+          $("#id_password1").val('')
+          $("#id_password2").val('')
+      })
+
+    })   

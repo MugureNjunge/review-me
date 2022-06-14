f2ccc1a728aec99241e55ec6614a8021d96ac1c8
MugureNjunge <maureennjunge22@gmail.com>
2022-06-14 07:43:43 +0300

    add a file to consume projects and profiles api

---
+++ consume.py
@@ -0,0 +1,7 @@
+import requests
+
+response = requests.get('http://127.0.0.1:8000/profiles')
+print(response.json())
+
+response = requests.get('http://127.0.0.1:8000/projects')
+print(response.json())
